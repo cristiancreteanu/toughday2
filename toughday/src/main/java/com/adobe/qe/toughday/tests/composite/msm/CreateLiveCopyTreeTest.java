d44e94ac5b72d5b0eceb48ff6df0f2f4c584ca9c
Andrei Dulvac <adulvac@adobe.com>
2016-12-14 17:06:51 +0200

    msm tests - todo

---
+++ toughday/src/main/java/com/adobe/qe/toughday/tests/composite/msm/CreateLiveCopyTreeTest.java
@@ -0,0 +1,13 @@
+package com.adobe.qe.toughday.tests.composite.msm;
+
+import com.adobe.qe.toughday.core.AbstractTest;
+import com.adobe.qe.toughday.core.CompositeTest;
+
+public class CreateLiveCopyTreeTest  extends CompositeTest {
+
+
+    @Override
+    public AbstractTest newInstance() {
+        return null;
+    }
+}
