b407b0f097ae59d4c0404527896331707406c3cc
Andrei Tuicu <tuicu@adobe.com>
2017-01-13 14:13:39 +0200

    @trivial - Fix Name Annotation

--- toughday/src/main/java/com/adobe/qe/toughday/core/annotations/Name.java
+++ toughday/src/main/java/com/adobe/qe/toughday/core/annotations/Name.java
@@ -1,8 +1,13 @@
 package com.adobe.qe.toughday.core.annotations;
 
-/**
- * Created by tuicu on 29/12/16.
- */
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(value = ElementType.TYPE)
 public @interface Name {
     /**
      * The name of the test class
