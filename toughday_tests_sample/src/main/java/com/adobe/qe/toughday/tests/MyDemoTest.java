6a14b9c90e8a8feed0e2922ff5e2750b1bea3af4
Andrei Dulvac <adulvac@adobe.com>
2017-03-28 17:28:57 +0200

    Added extra test jar sample

---
+++ toughday_tests_sample/src/main/java/com/adobe/qe/toughday/tests/MyDemoTest.java
@@ -0,0 +1,138 @@
+package com.adobe.qe.toughday.tests;
+
+import com.adobe.qe.toughday.core.AbstractTest;
+import com.adobe.qe.toughday.core.annotations.*;
+import com.adobe.qe.toughday.core.config.ConfigArgGet;
+import com.adobe.qe.toughday.core.config.ConfigArgSet;
+import com.adobe.qe.toughday.tests.sequential.SequentialTestBase;
+import org.apache.sling.testing.clients.ClientException;
+
+/**
+ * A demo test class that explains how tests must be written. It extends the SequentialTestBase,
+ * meaning will have the test method as defined there and it will be run by SequentialTestRunner.
+ * Since it is also a subtype of AbstractTest, it will be automatically picked up by the core
+ * and can be added to the suite from the command line with --add MyDemoTest Property=myValue
+ */
+@Description(desc = "Demo description")
+public class MyDemoTest extends SequentialTestBase {
+    private String property;
+
+    /**
+     *  Public constructor with no args.
+     *  This will be called when the test is instantiated by the configuration parser.
+     */
+    public MyDemoTest() {
+    }
+
+    /**
+     * Constructor with arguments to pass the internal field values when replicated
+     * to be called from the newInstance method that all tests must implement.
+     * You can write a constructor like this, or use the builder pattern in setters.
+     * @param property
+     */
+    public MyDemoTest(String property) {
+        this.property = property;
+    }
+
+
+    /**
+     *  Setup method.
+     *  This will run only once, before any instances of this test are ran.
+     */
+    @Setup
+    private void setupMethod() {
+        System.out.println(getFullName() + " Setup");
+        try {
+            /* Sleeps are not required, actually they are quite harmful, because they reduce the rate with which
+            the tests and affect the throughput. Use them only if the tests simulates wait between requests, otherwise
+            use the --WaitTime parameter to specify wait between test runs.
+             */
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  Before method.
+     *  This will run before every test run.
+     */
+    @Before
+    private void beforeMethod() {
+        System.out.println(getFullName() + " Before");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  After method
+     *  This will run after the test, even if the test fails
+     */
+    @After
+    private void afterMethod() {
+        System.out.println(getFullName() + " After");
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  Test method as defined by the SequentialTestBase. Different test bases
+     *  can define test methods differently. The runner of the test needs to know
+     *  how to properly run it. If none of the test bases are fitted for your particular
+     *  test, you can choose to extend AbstractTest, but you will have to implement the runner
+     *  for your new type of test as well.
+     */
+    @Override
+    public void test() throws ClientException {
+        System.out.println(getFullName() + " Running test with Property=" + property);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Command line argument property.
+     * Annotating your setter with ConfigArgSet will expose it to the command line as a property for this test.
+     * Properties annotated with ConfigArgSet are by default required, if the user doesn't give a value for it
+     * Toughday won't run. You can set the property to optional if you have a default for it
+     * @param property value as given by the user in the command line. Must be of type String
+     * @return usually tests respect the builder pattern and return "this", but it's not a requirement
+     */
+    @ConfigArgSet(/*required = true*/)
+    public MyDemoTest setProperty(String property) {
+        this.property = property;
+        return this;
+    }
+
+    /**
+     * Annotating your getter with ConfigArgGet will allow Toughday to obtain the configured value for a particular
+     * test instance once all configurations (code defaults, config file, cmd line, etc.) and log it/print it.
+     * @return
+     */
+    @ConfigArgGet
+    public String getProperty() {
+        return property;
+    }
+
+    /**
+     * Method called by the core in order to replicate this test for all the threads. Should return a
+     * new instance of this test with all the properties already set.
+     * You don't have to worry about the ID and the Name, those are taken care of.
+     * @return
+     */
+    @Override
+    public AbstractTest newInstance() {
+        //Call the constructor with arguments
+        return new MyDemoTest(property);
+        //or you can use the builder pattern
+        //return new MyDemoTest().setProperty(property);
+    }
+}
