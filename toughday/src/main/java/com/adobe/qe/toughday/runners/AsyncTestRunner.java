cbe081ce5a62f9a98a51719844d8d0cc7a1d888d
Andrei Tuicu <tuicu@adobe.com>
2017-10-20 18:23:05 +0300

    @trivial - Fix compilation failure

--- toughday/src/main/java/com/adobe/qe/toughday/runners/AsyncTestRunner.java
+++ toughday/src/main/java/com/adobe/qe/toughday/runners/AsyncTestRunner.java
@@ -1,9 +1,6 @@
 package com.adobe.qe.toughday.runners;
 
-import com.adobe.qe.toughday.core.AbstractTestRunner;
-import com.adobe.qe.toughday.core.ChildTestFailedException;
-import com.adobe.qe.toughday.core.RunMap;
-import com.adobe.qe.toughday.core.SkippedTestException;
+import com.adobe.qe.toughday.core.*;
 import com.adobe.qe.toughday.tests.async.AsyncTestBase;
 
 import java.lang.Throwable;
@@ -16,7 +13,7 @@ public class AsyncTestRunner extends AbstractTestRunner<AsyncTestBase> {
     }
 
     @Override
-    protected void run(AsyncTestBase testObject, RunMap runMap) throws Throwable {
+    protected void run(AsyncTestBase testObject, RunMap runMap) throws ToughDayException {
 
     }
 }
