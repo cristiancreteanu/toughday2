51f42a0980b9fc2e2d534dcab3544df86c284918
Andrei Tuicu <tuicu@adobe.com>
2018-03-22 17:58:54 +0200

    NPR-22781 - Investigate what it would need to run thoughday2 against https self signed certificate

--- aem-td2-core-tests/src/main/java/com/adobe/qe/toughday/tests/sequential/AEMTestBase.java
+++ aem-td2-core-tests/src/main/java/com/adobe/qe/toughday/tests/sequential/AEMTestBase.java
@@ -7,12 +7,20 @@ import org.apache.http.auth.AuthSchemeProvider;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.config.Lookup;
 import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.sling.testing.clients.ClientException;
 import org.apache.sling.testing.clients.SlingClient;
 import org.apache.sling.testing.clients.interceptors.FormBasedAuthInterceptor;
 
+import javax.net.ssl.SSLContext;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Base class for sequential tests.
@@ -38,7 +46,7 @@ public abstract class AEMTestBase extends SequentialTest {
      * @return the builder for either building the default client or overwriting properties before
      * @throws URISyntaxException
      */
-    public static SlingClient.Builder createClientBuilder(GlobalArgs args) throws URISyntaxException {
+    public static SlingClient.Builder createClientBuilder(GlobalArgs args) throws Exception {
         URIBuilder uriBuilder = new URIBuilder()
                 .setScheme(args.getProtocol())
                 .setHost(args.getHost())
@@ -52,6 +60,15 @@ public abstract class AEMTestBase extends SequentialTest {
         SlingClient.Builder builder = SlingClient.Builder.create(uriBuilder.build(),
                 args.getUser(),
                 args.getPassword());
+
+        if(!args.getHostValidationEnabled()) {
+            SSLContext sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                    .build();
+            SSLConnectionSocketFactory sslFactory = new SSLConnectionSocketFactory(sslContext, new NoopHostnameVerifier());
+            builder.httpClientBuilder().setSSLSocketFactory(sslFactory);
+        }
+
         if(args.getAuthMethod().equals("basic")) {
             //no-op
         } else if(args.getAuthMethod().equals("login-token")) {
@@ -64,7 +81,7 @@ public abstract class AEMTestBase extends SequentialTest {
         return builder;
     }
 
-    public static SlingClient createClient(GlobalArgs args) throws URISyntaxException, ClientException {
+    public static SlingClient createClient(GlobalArgs args) throws Exception {
         return createClientBuilder(args).build();
     }
 }
